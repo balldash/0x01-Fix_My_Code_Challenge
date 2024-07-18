d1d8039fd6aaf043e61a4c1ba1626fd4354702ba
Bislon Zulu <bislonzulu1@outlook.com>
2024-07-18 12:22:13 +0200

    task 0

--- status_server/api/v1/app.py
+++ status_server/api/v1/app.py
@@ -6,7 +6,8 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix="")
+print(app.url_map)
 
 
 @app.errorhandler(404)
